b24fa55fb1d1894797bd682533a04c0798cb6902
Erland <erland.vo@telenet.be>
2023-05-15 10:23:43 +0200

    updated api for revalidation of all pages

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,6 +1,7 @@
 // file pages/api/revalidate:
 
-// hooked as https://hot-yoga-ghent.vercel.app/api/revalidate
+// hooked as https://hot-yoga-ghent.vercel.app/api/revalidate?secret=stayhappy&page=timetable (or other page :) )
+// Local: try http://localhost:3000/api/revalidate?secret=stayhappy&page=timetable
 
 export default async function handler(req: any, res: any) {
   const { method, query } = req;
@@ -14,27 +15,47 @@ export default async function handler(req: any, res: any) {
 
   switch (method) {
     case "GET":
-      // res.status(200).json({ feedback: "GET request successfull" });
-      // break;
+    // res.status(200).json({ feedback: "GET request successfull" });
+    // break;
 
     case "POST":
-      console.log("revalidation triggered!");
-
       // Check for secret to confirm this is a valid request
-      if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
-        return res.status(401).json({ message: 'Invalid token' });
+      if (query.secret !== process.env.REVALIDATE_TOKEN) {
+        return res.status(401).json({
+          message: `Invalid token '${query.secret}', you are not allowed to revalidate page ${query.page}.`,
+        });
       }
-    
+
+      if (
+        (query.page === undefined) ||
+        ![
+          "",
+          "home",
+          "yoga",
+          "new-to-hot-yoga-ghent",
+          "timetable",
+          "pricing",
+          "testimonials",
+          "events",
+          "contact",
+          "blog",
+        ].includes(query.page)
+      ) {
+        return res.status(401).json({ message: `The page '${query.page}' does not exist` });
+      }
+
       try {
+        console.log(`Starting regeneration of page /${query.page}`);
+        
         // this should be the actual path not a rewritten path
         // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-        await res.revalidate("/timetable");
+        await res.revalidate(`/${query.page}`);
         return res.json({ revalidated: true });
       } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        console.log("revalidation failed:", err);
-        return res.status(500).send("Error revalidating");
+        console.log(`revalidation of page ${query.page} failed:`, err);
+        return res.status(500).send(`Error revalidating page ${query.page}`);
       }
       break;
 
