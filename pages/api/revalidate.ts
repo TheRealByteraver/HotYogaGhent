6933cc33881bea502b3de152cb6bebae90f6ec74
Erland <erland.vo@telenet.be>
2023-05-15 18:18:31 +0200

    reduced delay in revalidate()

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -92,7 +92,7 @@ export default async function handler(req: any, res: any) {
         // this should be the actual path not a rewritten path
         // e.g. for "/blog/[slug]" this should be "/blog/post-1"
         await res.revalidate(`/${query.page}`);
-        await setTimeout(10000); // let Vercel breathe a bit
+        // await setTimeout(1000); // let Vercel breathe a bit
         await res.revalidate(`${page}`);
 
         return res.json({ revalidated: true });
